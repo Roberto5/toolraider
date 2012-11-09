16fdbbb321134effb07c542760c3815ebd29551e
pagliaccio <mangheriniroberto@alice.it>
2012-11-09 20:10:46 +0100

    interface shiptool complete

--- common/js/ship.js
+++ common/js/ship.js
@@ -1,83 +1,196 @@
 $(function() {
-        var tabTitle = $( "#tab_title" ),
-            tabContent = $( "#tab_content" ),
-            tabTemplate = "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close'>Remove Tab</span></li>";
-        //crea la tabs
-        var tabs = $( ".tabs" ).tabs({
-        	collapsible: true,
-        });
- 
-        // modal dialog init: custom buttons and a "close" callback reseting the form inside
-        var dialog = $( "#dialog" ).dialog({
-            autoOpen: false,
-            modal: true,
-            width:1000,
-            buttons: {
-                Add: function() {
-                    addTab();
-                    $( this ).dialog( "close" );
-                },
-                Cancel: function() {
-                    $( this ).dialog( "close" );
-                }
-            },
-            close: function() {
-                form[ 0 ].reset();
-            }
-        });
- 
-        // addTab form: calls addTab function on submit and closes the dialog
-        var form = dialog.find( "form" ).submit(function( event ) {
-            addTab();
-            dialog.dialog( "close" );
-            event.preventDefault();
-        });
-        // actual addTab function: adds new tab using the input from the form above
-        function addTab() {
-            var label = tabTitle.find('option:selected').text(),
-                id = "planet" + tabTitle.val(),
-                li = $( tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label ) ),
-                tabContentHtml = tabContent.html();
-            tabContentHtml=tabContentHtml.replace(/"ship"/gi,'"readonly"');
-            tabContentHtml=tabContentHtml.replace('table class="readonly"','table class="ship"');
-            input=tabContent.find('input');
-        	for (key in input) {
-        		if (key=='length') break;
-        		v=$(input[key]);
-        		p=new RegExp('<span id="t'+key+'p0" class="readonly">[0-9]+');
-        		pid=id.replace('planet','');
-        		rep='<span id="t'+key+'p'+pid+'" class="readonly">'+v.val();
-        		tabContentHtml=tabContentHtml.replace(p,rep);
-        		p=new RegExp('id="i'+key+'p0" value="[0-9]+');
-        		rep='id="i'+key+'p'+pid+'" value="'+v.val();
-        		tabContentHtml=tabContentHtml.replace(p,rep);
-        		/*v.attr('value',v.val());
-        		v.removeClass('ship');
-        		v.addClass('readonly');
-        		spanid=v.attr('id').replace('i','t');
-        		$('#'+spanid).text(v.val()).removeClass('ship').addClass('readonly');*/
-        	}
-            tabs.find( ".ui-tabs-nav" ).append( li );
-            tabs.append( "<div id='" + id + "'><p>" + tabContentHtml + "</p></div>" );
-            tabs.tabs( "refresh" );
-            //ship.add();
-        }
- 
-        // addTab button: just opens the dialog
-        $( "#add_tab" )
-            .button()
-            .click(function() {
-                dialog.dialog( "open" );
-            });
-        tabs.find('form').live('submit',function(event){
-        	console.log('close edit');
-        	event.preventDefault();
-        });
-        // close icon: removing the tab on click
-        $( ".tabs span.ui-icon-close" ).live( "click", function() {
-        	//ship.del(ID_PLANET)
-            var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
-            $( "#" + panelId ).remove();
-            tabs.tabs( "refresh" );
-        });
-    });
+	ship.init();
+});
+
+var ship = {
+	inedit : new Array(),
+	tabTitle : null,
+	tabContent : null,
+	tabTemplate : null,
+	tabs : null,
+	dialog : null,
+	form : null,
+	init : function() {
+		this.tabTitle = $("#tab_title");
+		this.tabContent = $("#tab_content");
+		this.tabTemplate = "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close'>Remove Tab</span></li>";
+		// create a tabs
+		this.tabs = $(".tabs").tabs({
+			collapsible : true,
+		});
+		// modal dialog init: custom buttons and a "close" callback reseting the
+		// form inside
+		this.dialog = $("#dialog").dialog({
+			autoOpen : false,
+			modal : true,
+			width : 1000,
+			buttons : {
+				Add : function() {
+					ship.add();
+					$(this).dialog("close");
+				},
+				Cancel : function() {
+					$(this).dialog("close");
+				}
+			},
+			close : function() {
+				ship.form[0].reset();
+			}
+		});
+
+		// addTab form: calls addTab function on submit and closes the dialog
+		this.form = this.dialog.find("form").submit(function(event) {
+			event.preventDefault();
+			ship.add();
+			ship.dialog.dialog("close");
+		});
+		// actual addTab function: adds new tab using the input from the form
+		// above
+
+		// addTab button: just opens the dialog
+		$("#add_tab").button().click(function() {
+			ship.dialog.dialog("open");
+		});
+		this.tabs.find('form').live(
+				'submit',
+				function(event) {
+					event.preventDefault();
+					id = $(this).parent().parent().parent().parent().parent()
+							.attr('id').replace('planet', '');
+					console.log('close edit id ', id);
+					ship.edit(id);
+
+				});
+		// close icon: removing the tab on click
+		$(".tabs span.ui-icon-close").live("click", function() {
+			var panel=$(this).closest("li");
+			var panelId = panel.attr("aria-controls");
+			ship.del(panelId.replace('planet',''));
+			panel.remove();
+			$("#" + panelId).remove();
+			ship.tabs.tabs("refresh");
+			
+		});
+		$('button.ship.edit').live('click', function(event) {
+			pid = this.id.replace('edit', '');
+			console.log('id ', pid);
+			ship.edit(pid);
+		});
+	},
+	/**
+	 * edit a table
+	 * 
+	 * @param pid
+	 */
+	edit : function(pid) {
+		if (this.inedit[pid]) {
+			this.inedit[pid] = false;
+			$('#planet' + pid + ' button').button('option', 'icons', {
+				primary : "ui-icon-wrench"
+			});
+			input = $('#planet' + pid + ' input').removeClass('ship').addClass(
+					'readonly');
+			$('#planet' + pid + ' span.ship').removeClass('ship').addClass(
+					'readonly');
+			diff = new Array();
+			edit = false;
+			for (key in input) {
+				if (key == 'length')
+					break;
+				v = $(input[key]);
+				n = v.val();
+				input.eq(key).parent()[0].reset();
+				after = parseInt(v.attr('value'));
+				before = parseInt(n);
+				v.attr('value', n);
+				diff[key] = before - after;
+				id = v.attr('id').replace('i', 't');
+				$('span#' + id).text(parseInt(n));
+				if (diff[key] != 0)
+					edit = true;
+			}
+			this.updateTot(diff);
+			// add ajax call
+			if (edit) {
+				console.log('ajax call');
+			}
+		} else {
+			this.inedit[pid] = true;
+			$('#planet' + pid + ' input').removeClass('readonly').addClass(
+					'ship');
+			$('#planet' + pid + ' span:not("ui-icon")').removeClass('readonly')
+					.addClass('ship');
+			$('#planet' + pid + ' button').button('option', 'icons', {
+				primary : 'ui-icon-check'
+			});
+		}
+	},
+	/**
+	 * aggiorna il totale
+	 * 
+	 * @param Array
+	 *            diff
+	 */
+	updateTot : function(diff) {
+		span = $('div.tabs div:eq(0) span');
+		for (i in diff) {
+			if (i == 'length')
+				break;
+			t = parseInt(span.eq(i).text()) + parseInt(diff[i]);
+			span.eq(i).text(t);
+		}
+	},
+	add : function(pid) {
+		pid=parseInt(pid);
+		if (pid==0) return false;
+		var label = this.tabTitle.find('option:selected').text(), id = "planet"
+				+ this.tabTitle.val(), li = $(this.tabTemplate.replace(
+				/#\{href\}/g, "#" + id).replace(/#\{label\}/g, label)), tabContentHtml = this.tabContent
+				.html();
+		tabContentHtml = tabContentHtml.replace(/"ship"/gi, '"readonly"');
+		tabContentHtml = tabContentHtml.replace('table class="readonly"',
+				'table class="ship"');
+		input = this.tabContent.find('input');
+		diff=[];
+		for (key in input) {
+			if (key == 'length')
+				break;
+			v = $(input[key]);
+			diff.push(v.val());
+			p = new RegExp('<span id="t' + key + 'p0" class="readonly">[0-9]+');
+			pid = id.replace('planet', '');
+			rep = '<span id="t' + key + 'p' + pid + '" class="readonly">'
+					+ v.val();
+			tabContentHtml = tabContentHtml.replace(p, rep);
+			p = new RegExp('id="i' + key + 'p0" value="[0-9]+');
+			rep = 'id="i' + key + 'p' + pid + '" value="' + v.val();
+			tabContentHtml = tabContentHtml.replace(p, rep);
+			tabContentHtml = tabContentHtml.replace('edit0', 'edit' + pid);
+			v.val(0);
+		}
+		this.tabs.find(".ui-tabs-nav").append(li);
+		this.tabs.append("<div id='" + id + "'>" + tabContentHtml + "</div>");
+		this.tabs.tabs("refresh");
+		//update totale
+		this.updateTot(diff);
+		//remove planet from list
+		this.tabTitle[0].remove(this.tabTitle[0].selectedIndex);
+		//ajax call
+	},
+	del:function(pid) {
+		input=$('#planet'+pid+' input');
+		diff=[];
+		for (key in input) {
+			if (key == 'length')
+				break;
+			v = $(input[key]);
+			diff.push(-parseInt(v.val()));
+		}
+		this.updateTot(diff);
+		//add planet to list
+		
+		this.tabTitle[0].options[this.tabTitle[0].options.length]=new Option($('a[href=#planet'+pid+']').text(),pid);
+		
+		//ajax call
+	}
+};
